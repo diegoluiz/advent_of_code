6e6071ac7aad262b9ada418ef79b86c42ffa9bfa
Diego Luiz <diegorcl@gmail.com>
2020-12-05 10:16:19 +0000

    AoC 2020 - Execute all days script

---
+++ 2020/all.js
@@ -0,0 +1,15 @@
+const { performance } = require('perf_hooks');
+const fs = require('fs');
+
+const dirs = fs.readdirSync('.')
+    .filter(x => x.startsWith('d'));
+
+const start = performance.now();
+dirs.forEach(d => {
+    console.log(`Dir: ${d}`);
+    const app = require(`./${d}/app.js`);
+    console.log('');
+});
+
+
+console.log(`Finished in ${performance.now() - start}`);
